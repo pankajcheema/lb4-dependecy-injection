e01e8835b91d0dd8b91c62eb8f785dbe60aafafa
pankaj cheema <pankajcheema1990@gmail.com>
2020-03-13 18:03:20 +0530

    first commit

--- src/custom-service/test-service.ts
+++ src/custom-service/test-service.ts
@@ -1,8 +1,23 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  repository,
+  Where,
+} from '@loopback/repository';
+import { UserRepository } from '../repositories';
+import { Context, inject } from '@loopback/context';
+
 export class Person {
+  public contextclas = new Context()
   constructor() {
+
 
   }
   async test(): Promise<void> {
     console.log("name pankaj");
+    const lt: UserRepository = this.contextclas.getSync('userrepo');
+    await lt.count();
+
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import { Lb4AppApplication } from './application';
 import { ApplicationConfig } from '@loopback/core';
 import { Person } from './custom-service/test-service';
-
+import { UserRepository } from './repositories';
+import { Context, inject } from '@loopback/context';
 export { Lb4AppApplication };
 
 export async function main(options: ApplicationConfig = {}) {
   const app = new Lb4AppApplication(options);
   await app.boot();
   await app.start();
+
+  //injecting dependency here
+
+
+
+  const context = new Context();
+  context.bind('userrepo').toClass(UserRepository);
+
+
   console.log("pankaj");
-  const per = new Person();
-  await per.test();
+  const ld = new Person(lt);
+
+  //dependency injection done
 
 
   const url = app.restServer.url;
